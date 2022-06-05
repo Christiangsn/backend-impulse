6cf11ae7210a29e08ccd824d49b3571d7fcb81e7
christiangsn <christianguimaraes1996@gmail.com>
2022-06-05 17:19:29 -0300

    feat: criação das classes e interfaces de feedbacks Repository

--- src/domain/Data/FeedBacks/CreateFeedBackRepository.ts
+++
@@ -1,9 +0,0 @@
-interface FeedBackCreateData {
-    type: string
-    comment: string
-    screenshot: string
-}
-
-export interface FeedBacksRepository {
-    create: ({ type, comment, screenshot}: FeedBackCreateData) => void
-}
