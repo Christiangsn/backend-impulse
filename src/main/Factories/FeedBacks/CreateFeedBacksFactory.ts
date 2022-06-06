807388bf1cb6ffd10270df37788005cbe233cb14
christiangsn <christianguimaraes1996@gmail.com>
2022-06-05 21:42:28 -0300

    test: start do servidor e uma request

--- src/main/Factories/FeedBacks/CreateFeedBacksFactory.ts
+++ src/main/Factories/FeedBacks/CreateFeedBacksFactory.ts
@@ -3,8 +3,6 @@ import { SubmitFeedBacksController } from '@presentation/useCases/FeedBacks/Subm
 import { SubmitFeedBacksServices } from '@presentation/useCases/FeedBacks/SubmitFeedBacksUseCase/SubmitFeedBacksServices';
 import { PrismaFeedBacksRepository } from './../../../core/Prisma/PrismaFeedBacksRepository/PrismaFeedBacksRepository';
 
-
-
 export const CreateFeedBacksFactory = (): IUseCaseControllers => {
     const createFeedBacksRepository = new PrismaFeedBacksRepository()
     const feedBackService = new SubmitFeedBacksServices(createFeedBacksRepository)
