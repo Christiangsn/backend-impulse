b730de70b3dd6aaf6f6b7b9aa592b3361f75ef4e
christiangsn <christianguimaraes1996@gmail.com>
2022-06-05 22:09:16 -0300

    feat: segregado a camada de useCases com as interfaces do Nodemailer

--- src/presentation/useCases/FeedBacks/SubmitFeedBacksUseCase/SubmitFeedBacksServices.ts
+++ src/presentation/useCases/FeedBacks/SubmitFeedBacksUseCase/SubmitFeedBacksServices.ts
@@ -1,3 +1,4 @@
+import { IMailAdapter } from '@domain/Adapters/Mail/IMailAdapter'
 import { ICreateFeedBackRepository } from "@domain/Data/FeedBacks/ICreateFeedBackRepository"
 import { IServiceResponse } from "@presentation/Contracts/useCases"
 import { IDTOSubmitFeedbacks, IDTOSubmitFeedbacksServices } from "./SubmitFeedBacksDTO"
@@ -5,11 +6,21 @@ import { IDTOSubmitFeedbacks, IDTOSubmitFeedbacksServices } from "./SubmitFeedBa
 export class SubmitFeedBacksServices implements IDTOSubmitFeedbacksServices {
     
     constructor(
-        private createFeedbacksRepository: ICreateFeedBackRepository
+        private createFeedbacksRepository: ICreateFeedBackRepository,
+        private mailAdapter: IMailAdapter
     ){}
  
     async execute ({ type, comment, screenshot }: IDTOSubmitFeedbacks): Promise<IServiceResponse> {
         await this.createFeedbacksRepository.create({ type, comment, screenshot })
+        await this.mailAdapter.sendMail({ 
+            subject: 'Novo FeedBack',
+            body: [
+                `<div style="font-family: san-serif; font-size: 16px; color: #111;">`,
+                `<p>Tipo do feedback: ${type} </p>`,
+                `<p>Comentário: ${comment} </p>`,
+                `/div>`
+            ].join('\n')
+        })
 
         return {
             body: 'FeedBack Criado com Sucesso',
